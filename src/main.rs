b7931be54f45c9f13da8a9d6372cf10724fd2d75
Max Cohn <mxc1805@rit.edu>
2019-08-06 22:23:59 -0400

    Added creation of database on load

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@
 
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+extern crate rusqlite;
+
 use rocket_contrib::templates::Template;
 use rocket_contrib::serve::StaticFiles;
-//use rocket_contrib::templates::Context;
+
 use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
 
-
-
-
+use rusqlite::types::ToSql;
+use rusqlite::{Connection, Result, NO_PARAMS};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Quote {
@@ -26,6 +32,23 @@ impl Quote {
     }
 }
 
+/// Query database for all quotes
+fn get_quotes() -> Vec<Quote>{
+    let conn = Connection::open("test.db").expect("Failed to open database");
+
+    // create 'quotes' table if it doesn't exist.
+    if conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='quotes'", params![]).is_err() {
+        conn.execute("CREATE TABLE quotes (quote TEXT NOT NULL, author TEXT NOT NULL);", params![])
+            .expect("Failed to create table 'quotes'");
+    }
+    
+    
+    // insert quotes
+    conn.execute("INSERT INTO quotes (quote, author) VALUES (?1,?2)", params!["asd","dsa"]).unwrap();
+
+
+    Vec::new()
+}
 
 #[get("/")]
 fn index() -> Template {
@@ -33,23 +56,26 @@ fn index() -> Template {
     let quotes = vec![Quote::new("t_asd", "auth")];
     // add them to a hashmap so we can serialize it as json with a key of "quotes"
     // and value of our list of quote objects
-    let mut hm = std::collections::HashMap::new();
+    let mut hm = HashMap::new();
     hm.insert("quotes", quotes);
     
     // render the template with the given context
-    Template::render("index", &hm)
+    return Template::render("index", &hm);
+}
+
+#[post("/newquote")]
+fn new_quote() {
+    //TODO get body of the POST request
+
+    // insert into database
 }
 
 fn main() {
-    let quotes = vec![Quote::new("t_asd", "auth")];
-    let mut hm = std::collections::HashMap::new();
-    hm.insert("quotes", quotes);
-    let serial = serde_json::to_string(&hm).unwrap();
-    println!("{}", serial);
-    
-    
+
+    let _a = get_quotes();
     
     rocket::ignite().attach(Template::fairing())
-        .mount("/", routes![index])
-        .mount("/static", StaticFiles::from("static")).launch();
+        .mount("/", routes![index]) // standard routes 
+        .mount("/static", StaticFiles::from("static")) // route for static content
+        .launch();
 }
