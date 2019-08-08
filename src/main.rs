731546dd999682ce94c6212f5227b3c0fa0ffea2
Max Cohn <mxc1805@rit.edu>
2019-08-07 21:25:02 -0400

    Updaed get)quotes() to better use databases

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ use std::collections::HashMap;
 use rusqlite::types::ToSql;
 use rusqlite::{Connection, Result, NO_PARAMS};
 
+
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Quote {
     text: String,
@@ -33,29 +35,39 @@ impl Quote {
 }
 
 /// Query database for all quotes
+/// 
+/// # Panics
+/// Panics on failure to open or read database
 fn get_quotes() -> Vec<Quote>{
     let conn = Connection::open("test.db").expect("Failed to open database");
-
-    // create 'quotes' table if it doesn't exist.
-    if conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='quotes'", params![]).is_err() {
-        conn.execute("CREATE TABLE quotes (quote TEXT NOT NULL, author TEXT NOT NULL);", params![])
-            .expect("Failed to create table 'quotes'");
-    }
     
-    
-    // insert quotes
-    conn.execute("INSERT INTO quotes (quote, author) VALUES (?1,?2)", params!["asd","dsa"]).unwrap();
+    // query all quotes. We assume that the quote's table exists
+    let mut stmt = conn.prepare("SELECT * FROM quotes")
+        .expect("Error reading from quotes. Check if the database exists.");
 
+    // get an iterator over Result<Quote> (required by rusqlite)
+    let quote_iter = stmt.query_map(NO_PARAMS, |row|
+        Ok(Quote {
+            text: row.get(0).unwrap(),
+            author: row.get(1).unwrap()
+        })
+    ).expect("Error reading from quotes. Check if the database exists.");
 
-    Vec::new()
+    // convert iterator into vector
+    quote_iter.map(|r| r.unwrap()).collect()
 }
 
 #[get("/")]
 fn index() -> Template {
     // create a vector of all the quotes
-    let quotes = vec![Quote::new("t_asd", "auth")];
+    let quotes = get_quotes();
+
     // add them to a hashmap so we can serialize it as json with a key of "quotes"
     // and value of our list of quote objects
+
+    // a quote serializes to {"text":"text", "author":"author"}
+    // by adding it to a hashmap, we get {"quotes": [{"text":"text", "author":"author"}]}
+    // which is what we need to use for the templating engine
     let mut hm = HashMap::new();
     hm.insert("quotes", quotes);
     
